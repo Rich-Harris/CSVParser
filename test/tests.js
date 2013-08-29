2a2d7627277d0266dcbb9ebc472bcbac7d2813dd
Rich Harris <richard.a.harris@gmail.com>
2013-08-29 17:33:42 +0100

    more tests

--- test/tests.js
+++ test/tests.js
@@ -23,6 +23,13 @@
 		t.deepEqual( parser.json(), [{ one: 1, two: 2, three: 3 }] );
 	});
 
+	test( 'Tab-separated data with initialisation option', function ( t ) {
+		var parser = new CSVParser( 'one\ttwo\tthree\n1\t2\t3', { delimiter: '\t' });
+
+		t.deepEqual( parser.array(), [ [ 'one', 'two', 'three' ], [ 1, 2, 3 ] ] );
+		t.deepEqual( parser.json(), [{ one: 1, two: 2, three: 3 }] );
+	});
+
 	test( 'Space-separated data', function ( t ) {
 		var parser = new CSVParser( 'one two three\n1 2 3' ).delimiter( ' ' );
 
@@ -44,7 +51,7 @@
 		t.deepEqual( parser.json(), [{ 'with\nnewline': 1, two: 2, three: 3 }] );
 	});
 
-	test( 'Malformed data throws an error', function ( t ) {
+	test( 'Malformed data throws an error in strict mode', function ( t ) {
 		var parser = new CSVParser( 'one,two,three\n1,2,3,4' ), errorThrown;
 
 		try {
@@ -56,6 +63,36 @@
 		}
 
 		ok( errorThrown );
+	});
+
+	test( 'Malformed data doesn\'t throw an error in non-strict mode', function ( t ) {
+		var parser = new CSVParser( 'one,two,three\n1,2,3,4', { strict: false }), errorThrown;
+
+		try {
+			parser.array();
+		}
+
+		catch ( err ) {
+			errorThrown = true;
+		}
+
+		ok( !errorThrown );
+	});
+
+	test( 'Qualifiers in cells are only treated as qualifiers if they are followed by a delimiter or newline', function ( t ) {
+		var parser = new CSVParser( 'one,two,three"four"five,"s"i"x"\n1,2,3"4"5,"6"' );
+
+		t.deepEqual( parser.array(), [ [ 'one', 'two', 'three"four"five', 's"i"x' ], [ 1, 2, '3"4"5', 6 ] ] );
+		t.deepEqual( parser.json(), [{ one: 1, two: 2, 'three"four"five': '3"4"5', 's"i"x': 6 }] );
+	});
+
+	test( 'Can initialise with options as the first (and only) argument', function ( t ) {
+		var parser = new CSVParser({ delimiter: '\t' });
+
+		parser.data( 'one\ttwo\tthree\n1\t2\t3' );
+
+		t.deepEqual( parser.array(), [ [ 'one', 'two', 'three' ], [ 1, 2, 3 ] ] );
+		t.deepEqual( parser.json(), [{ one: 1, two: 2, three: 3 }] );
 	});
 
 }());
